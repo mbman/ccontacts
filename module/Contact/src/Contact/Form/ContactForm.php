5cc492a421138ce3ef21758d749ec72833e63506
mbman <mario.baricevic@gmail.com>
2013-09-05 19:14:43 +0200

    Contact's zend form

---
+++ module/Contact/src/Contact/Form/ContactForm.php
@@ -0,0 +1,114 @@
+<?php
+namespace Contact\Form;
+
+use Zend\Form\Form;
+use DoctrineORMModule\Stdlib\Hydrator\DoctrineEntity as DoctrineHydrator;
+
+class ContactForm extends Form
+{
+
+    public function __construct($entityManager)
+    {
+        parent::__construct('contact');
+        $this->setAttribute('method', 'post');
+
+        $hydrator = new DoctrineHydrator($entityManager,'Contact\Entity\Contact');
+        $this->setHydrator($hydrator);
+
+        $this->add(array(
+            'name' => 'id',
+            'attributes' => array(
+                'type'  => 'hidden',
+            ),
+        ));
+
+        $this->add(array(
+            'name' => 'lastName',
+            'attributes' => array(
+                'type'  => 'text',
+            ),
+            'options' => array(
+                'label' => 'Last name',
+            ),
+        ));
+
+        $this->add(array(
+            'name' => 'firstName',
+            'attributes' => array(
+                'type'  => 'text',
+            ),
+            'options' => array(
+                'label' => 'First name',
+            ),
+        ));
+        $this->add(array(
+            'name' => 'job',
+            'attributes' => array(
+                'type'  => 'text',
+            ),
+            'options' => array(
+                'label' => 'Job title',
+            ),
+        ));
+        $this->add(array(
+            'name' => 'address',
+            'type' => 'text',
+            'attributes' => array(
+            ),
+            'options' => array(
+                'label' => 'Address',
+            ),
+        ));
+        $this->add(array(
+            'name' => 'city',
+            'attributes' => array(
+                'type'  => 'text',
+            ),
+            'options' => array(
+                'label' => 'City',
+            ),
+        ));
+        $this->add(array(
+            'name' => 'state',
+            'attributes' => array(
+                'type'  => 'text',
+            ),
+            'options' => array(
+                'label' => 'State/Province',
+            ),
+        ));
+        $this->add(array(
+            'name' => 'zip',
+            'attributes' => array(
+                'type'  => 'text',
+            ),
+            'options' => array(
+                'label' => 'ZIP',
+            ),
+        ));
+        $this->add(array(
+            'name' => 'country',
+            'attributes' => array(
+                'type'  => 'text',
+            ),
+            'options' => array(
+                'label' => 'Country/Region',
+            ),
+        ));
+        $this->add(array(
+            'name' => 'notes',
+            'type' => 'Textarea',
+            'options' => array(
+                'label' => 'Notes',
+            ),
+        ));
+        $this->add(array(
+            'name' => 'submit',
+            'attributes' => array(
+                'type'  => 'submit',
+                'value' => 'Save',
+                'id' => 'submitbutton',
+            ),
+        ));
+    }
+}
