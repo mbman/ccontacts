06c309244a42a32acc2a01e46650e97a02344cbf
mbman <mario.baricevic@gmail.com>
2013-09-05 23:53:04 +0200

    Contact Form error handling

--- public/js/views/contact.js
+++ public/js/views/contact.js
@@ -70,9 +70,27 @@ window.ContactFormView = Backbone.View.extend({
                 app.navigate("contact/"+model.id, true);
             },
             error: function (model, response) {
-                $.each(response.responseJSON);
+                var $form = $("#contactform");
+                _.each(response.responseJSON, function (errors, key) {
+                    var $el = $("[data-el='"+key+"']", this);
+                    $el.addClass("has-error")
+                       .append(new ContactFormHelpView({model:errors}).render().el);
+                }, $form);
             }
-        })
+        });
+    },
+
+});
+
+
+window.ContactFormHelpView = Backbone.View.extend({
+
+    tagName: "span",
+    className: "help-block pull-right",
+
+    render:function () {
+        $(this.el).html(_.values(this.model).join("<br>"));
+        return this;
     },
 
 });
